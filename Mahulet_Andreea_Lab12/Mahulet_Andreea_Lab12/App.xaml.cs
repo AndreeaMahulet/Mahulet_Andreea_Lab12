6fa1faea7ce8aea463f9171c832f66e6b6fd4cee
ANDREEA MĂHULEȚ <andreea.mahulet@stud.ubbcluj.ro>
2021-01-06 19:08:09 +0200

    Add project files.

---
+++ Mahulet_Andreea_Lab12/Mahulet_Andreea_Lab12/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Mahulet_Andreea_Lab12.Data;
+namespace Mahulet_Andreea_Lab12
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+    }
+
+     
+}
---
+++ Mahulet_Andreea_Lab12/Mahulet_Andreea_Lab12/Data/IRestService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Mahulet_Andreea_Lab12.Models;
+namespace Mahulet_Andreea_Lab12.Data
+{
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+    }
+}
